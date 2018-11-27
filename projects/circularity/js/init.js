41618f69bff50df154442ddc318799c03b2f72c2
Novalee Schroeder <nts2308@ppsbstudent.org>
2018-11-27 00:49:57 +0000

    finish todo1 part1

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,106 +1,100 @@
 var init = function (window) {
-    'use strict';
-    var 
-        draw = window.opspark.draw,
-        physikz = window.opspark.racket.physikz,
-        
-        app = window.opspark.makeApp(),
-        canvas = app.canvas, 
-        view = app.view,
-        fps = draw.fps('#000');
-        
-    
-    window.opspark.makeRunner = function() {
-        
-        window.opspark.runner = {};
-        var runner = window.opspark.runner;
-        
-        ////////////////////////////////////////////////////////////////
-        // ALL CODE GOES BELOW HERE                                   //
-        ////////////////////////////////////////////////////////////////
-        
-        // TODO 1 : Declare and initialize our variables //
-        var circle;
-        var circles = [];
-        
-        // TODO 2 : Create a function that draws a circle  //
-        var drawCircle = function() {
-         circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas);
-        view.addChild(circle);
-        circles.push(circle);
-        // other code...
-        }
-        
+  'use strict';
+  var
+      draw = window.opspark.draw,
+      physikz = window.opspark.racket.physikz,
 
-        // TODO 3 : Call the drawCircle function 5 times //
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
-        drawCircle()
+      app = window.opspark.makeApp(),
+      canvas = app.canvas,
+      view = app.view,
+      fps = draw.fps('#000');
 
-        // TODO 7 : Create a Loop to call drawCircle 100 times
 
-    
-        view.addChild(fps);
-        app.addUpdateable(fps);
-    
-        runner.checkCircleBounds = function(circle) {
-            // TODO 5 : YOUR CODE STARTS HERE //////////////////////
-            
-            // if the circle has gone out the right side of the screen then place it off-screen left
-            if ( circle.x > canvas.width + circle.radius ) {
-                circle.x = 0 - circle.radius;
-            } 
-            // TODO 5a) if the circle has gone out of the left side of the screen then place it off-screen right
-            else if ( / * Fill me in! * / ) {
-                
-            } 
+  window.opspark.makeRunner = function() {
 
-            // TODO 5b) if the circle has gone out of the top side of the screen then place it off-screen bottom
-            if ( / * Fill me in! * / ) {
-                
-            }
-            // TODO 5c) if the circle has gone out of the bottom side of the screen then place it off-screen top 
-            else if ( / * Fill me in! * / ) {
-            
-            }
-            // YOUR TODO 5 CODE ENDS HERE //////////////////////////
-        }
-    
-        var update = function() {
-            // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
-            
-            // TODO 6 : Call checkCircleBounds on your circles.
-           
+      window.opspark.runner = {};
+      var runner = window.opspark.runner;
 
-            // TODO 8 : Iterate over the array
-           
+      ////////////////////////////////////////////////////////////////
+      // ALL CODE GOES BELOW HERE                                   //
+      ////////////////////////////////////////////////////////////////
 
-        }
-        
-        ////////////////////////////////////////////////////////////////////
-        // NO CODE BELOW HERE                                             //
-        ////////////////////////////////////////////////////////////////////
-        
-        runner.circle = circle;
-        runner.circles = circles;
-        runner.drawCircle = drawCircle;
-        runner.update = update;
-        
-        app.addUpdateable(window.opspark.runner);
-    }
+      // TODO 1 : Declare and initialize our variables //
+      var circle;
+      var circles = [];
+
+      // TODO 2 : Create a function that draws a circle  //
+      var drawCircle = function() {
+          circle = draw.randomCircleInArea(canvas, true, false, 'cyan', 2);
+          physikz.addRandomVelocity(circle, canvas);
+          view.addChild(circle);
+          circles.push(circle);
+      };
+
+      // TODO 3 : Call the drawCircle function 5 times //
+
+      // TODO 7 : Create a Loop to call drawCircle 100 times
+      for (var d = 0; d <= 100; d++) {
+          drawCircle(d);
+      }
+
+      view.addChild(fps);
+      app.addUpdateable(fps);
+
+      runner.checkCircleBounds = function(circle) {
+          // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+
+          // if the circle has gone out the right side of the screen then place it off-screen left
+          if ( circle.x > canvas.width + circle.radius ) {
+              circle.x = 0 + circle.radius;
+          }
+          // TODO 5a) if the circle has gone out of the left side of the screen then place it off-screen right
+          else if ( circle.x < 0 + circle.radius ) {
+              circle.x = canvas.width - circle.radius;
+          }
+
+          // TODO 5b) if the circle has gone out of the top side of the screen then place it off-screen bottom
+          if ( circle.y < 0 + circle.radius ) {
+              circle.y = canvas.height - circle.radius;
+          }
+          // TODO 5c) if the circle has gone out of the bottom side of the screen then place it off-screen top
+          else if ( circle.y > canvas.height + circle.radius ) {
+              circle.y = 0 + circle.radius;
+          }
+          // YOUR TODO 5 CODE ENDS HERE //////////////////////////
+      };
+
+      var update = function() {
+          // TODO 4 : Update the circle's position //
+
+          // TODO 6 : Call checkCircleBounds on your circles.
+
+          // TODO 8 : Iterate over the array
+      for (var p = 0; p <= 100; p++) {
+          physikz.updatePosition(circles[p]);
+      }
+      for (var r = 0; r <= 100; r++) {
+          runner.checkCircleBounds(circles[r]);
+      }
+
+      };
+
+      ////////////////////////////////////////////////////////////////////
+      // NO CODE BELOW HERE                                             //
+      ////////////////////////////////////////////////////////////////////
+
+      runner.circle = circle;
+      runner.circles = circles;
+      runner.drawCircle = drawCircle;
+      runner.update = update;
+
+      app.addUpdateable(window.opspark.runner);
+  }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports = init;
+  (typeof process.versions.node !== 'undefined')) {
+  // here, export any references you need for tests //
+  module.exports = init;
 }
